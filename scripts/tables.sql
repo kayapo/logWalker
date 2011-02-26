4a846b5c881a347d037b05abe40188d0ca6a7113
kayapo <molnaromatic@gmail.com>
2011-02-26 09:36:42 +0100

    Removal of on unnecesary tables

--- scripts/tables.sql
+++ scripts/tables.sql
@@ -24,21 +24,4 @@ CREATE TABLE IF NOT EXISTS `logs` (
   FULLTEXT KEY `idx_msg` (`message`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 DEFAULT COLLATE=utf8_general_ci CHECKSUM=1;
 
-
--- create table named hosts
-CREATE TABLE IF NOT EXISTS `hosts` (
-  `id` int(4) unsigned NOT NULL AUTO_INCREMENT,
-  `host` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `idx_host` (`host`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 CHECKSUM=1;
-
--- create table named tags
-CREATE TABLE IF NOT EXISTS `tags` (
-  `id` int(4) unsigned NOT NULL AUTO_INCREMENT,
-  `tag` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `idx_tag` (`tag`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 CHECKSUM=1;
-
 -- Create script END ;)
